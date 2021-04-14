53a0d1b5376f58df0044275ecbcd48c8bf3d5d53
Sahir Patel <sahirmpatel@gmail.com>
2021-04-14 12:27:21 +0530

    core code

---
+++ README.md
@@ -0,0 +1,16 @@
+Simple way to trim your text content as per your given limit
+
+`npm i react-word-cutter --save`
+
+After installing...
+
+```
+import WordCutter from "react-word-cutter"
+
+<WordCutter limit={10} >
+Lorem ipsum
+</WordCutter>
+
+```
+
+## Options
---
+++ src/WordCutter.js
@@ -0,0 +1,14 @@
+const WordCutter = (props) => {
+  let inputtext = props.children;
+  let wordlimit = props.limit;
+  let result;
+  if (inputtext.length < wordlimit || !wordlimit) {
+    result = inputtext;
+  } else {
+    result = `${inputtext.slice(0, wordlimit)}...`;
+  }
+
+  return <span title={props.children}>{result}</span>;
+};
+
+export default WordCutter;
